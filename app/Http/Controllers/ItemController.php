4f19a20308f8a848ce21c2572f71b33f3b40e77f
Lukman Imran <amirrullukmanhaziq@gmail.com>
2022-11-30 13:59:27 +0800

    Create search function

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ItemController extends Controller
 {
@@ -14,6 +15,9 @@ class ItemController extends Controller
 
     public function store(Request $request)
     {
+        $slug = Str::slug($request->name);
+        $request->merge(['slug' => $slug]);
+
         $request->validate([
             'name' => 'required',
             'slug' => 'required'
@@ -22,20 +26,34 @@ class ItemController extends Controller
         return Item::create($request->all());
     }
 
-    public function show($id)
+    public function show(Item $item)
     {
-        return Item::find($id);
+        return Item::find($item);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Item $item)
     {
-        $item = Item::find($id);
+        if($request->name) {
+            $slug = Str::slug($request->name);
+            $request->merge(['slug' => $slug]);
+        }
+
+        $item = Item::find($item->id);
         $item->update($request->all());
         return $item;
     }
 
-    public function destroy($id)
+    public function destroy(Item $item)
     {
-        return Item::destroy($id);
+        $item = Item::find($item);
+        return Item::destroy($item);
+    }
+
+    public function search($string)
+    {        
+        $item = Item::where('name', 'like', '%' . $string . '%')->orWhere('description', 'like', '%' . $string . '%')
+        ->orWhere('slug', 'like', '%' . $string . '%')->orWhere('created_at', 'like', '%' . $string . '%')->get();
+        
+        return $item;
     }
 }
